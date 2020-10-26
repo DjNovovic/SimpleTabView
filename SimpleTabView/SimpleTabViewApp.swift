a513e4a1bcbad2091fe488ba94523fd6a0982ac9
DjNovovic <djordjijenovovic@gmail.com>
2020-10-26 18:47:03 +0100

    Initial Commit

---
+++ SimpleTabView/SimpleTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTabViewApp.swift
+//  SimpleTabView
+//
+//  Created by Djordjije on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
